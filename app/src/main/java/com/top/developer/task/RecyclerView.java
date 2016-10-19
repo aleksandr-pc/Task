5d9e5a3abe99e5619a8fa82e608c2a3544bb2199
developer <bortsov.nuos@gmail.com>
2016-10-19 23:30:30 +0300

    create main activity and recyclerview

---
+++ app/src/main/java/com/top/developer/task/RecyclerView.java
@@ -0,0 +1,19 @@
+package com.top.developer.task;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by developer on 10/19/2016.
+ */
+
+public class RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forrecyclerview);
+    }
+
+
+}
